2f5d3e77476b86e7f5aec82cdcf1fb4445c6483e
Tamal Saha <tamal@appscode.com>
2021-06-11 15:50:07 -0700

    added-all

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module github.com/tamalsaha/ssa-demo
 
 go 1.16
+
+require (
+	github.com/google/go-cmp v0.5.4 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/googleapis/gnostic v0.5.4 // indirect
+	github.com/imdario/mergo v0.3.11 // indirect
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	k8s.io/api v0.21.1
+	k8s.io/apimachinery v0.21.1
+	k8s.io/client-go v0.21.1
+	k8s.io/utils v0.0.0-20210111153108-fddb29f9d009 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,53 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"log"
+	"path/filepath"
 
+	core "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	corev1 "k8s.io/client-go/applyconfigurations/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+func main() {
+	masterURL := ""
+	kubeconfigPath := filepath.Join(homedir.HomeDir(), ".kube", "config")
+
+	config, err := clientcmd.BuildConfigFromFlags(masterURL, kubeconfigPath)
+	if err != nil {
+		log.Fatalf("Could not get Kubernetes config: %s", err)
+	}
+
+	client := kubernetes.NewForConfigOrDie(config)
+
+	p := corev1.Pod("busybox", "default").
+		WithLabels(map[string]string{
+			"app": "busybox",
+		}).WithSpec(corev1.PodSpec().
+		WithRestartPolicy(core.RestartPolicyAlways).
+		WithContainers(corev1.Container().
+			WithImage("ubuntu:20.04").
+			WithImagePullPolicy(core.PullIfNotPresent).
+			WithName("busybox").
+			WithCommand("sleep", "300").
+			WithResources(corev1.ResourceRequirements().
+				WithLimits(core.ResourceList{
+					core.ResourceCPU:    resource.MustParse("500m"),
+					core.ResourceMemory: resource.MustParse("1Gi"),
+				}))))
+
+	p2, err := client.CoreV1().Pods("default").Apply(context.Background(), p, metav1.ApplyOptions{
+		Force:        false,
+		FieldManager: "tamal",
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v", p2)
 }
